095bf75d64a1858d079e90ef70cc889786590fc1
rnhkz <alb190010@utdallas.edu>
2023-04-03 11:52:00 -0500

    Added feature to change row, column, and mine variables using command arguments. Changed variable names to better reflect utility.

--- PySweeper.py
+++ PySweeper.py
@@ -4,7 +4,7 @@ import sys
 
 pg.init()
 
-def get_new_revealed_game_board(gb, gb_is_revealed, row, col, rows, columns):
+def get_new_revealed_game_board(gb, gb_is_revealed, row, col, rows, cols):
     stack = [[row, col]]
     while len(stack) > 0:
         cur_tile = stack.pop()
@@ -16,12 +16,12 @@ def get_new_revealed_game_board(gb, gb_is_revealed, row, col, rows, columns):
                     if col-1 >= 0:
                         stack.append([row-1, col-1])
                     stack.append([row-1, col])
-                if col+1 < columns:
+                if col+1 < cols:
                     if row-1 >= 0:
                         stack.append([row-1, col+1])
                     stack.append([row, col+1])
                 if row+1 < rows:
-                    if col+1 < columns:
+                    if col+1 < cols:
                         stack.append([row+1, col+1])
                     stack.append([row+1, col])
                 if col-1 >= 0:
@@ -33,9 +33,22 @@ def get_new_revealed_game_board(gb, gb_is_revealed, row, col, rows, columns):
     return gb_is_revealed
 
 def main():
-    size = rows, columns = 10, 10
-    game_board = [[0 for x in range(rows)] for y in range(columns)]
-    game_board_is_revealed = [[0 for x in range(rows)] for y in range(columns)]
+    rows, cols = 10, 10
+    total_mines = 10
+
+    if len(sys.argv) == 4:
+        rows = int(sys.argv[1])
+        cols = int(sys.argv[2])
+        total_mines = int(sys.argv[3])
+        if total_mines > (rows * cols) * .75:
+            total_mines = int((rows * cols) * .1)
+            print(f"Mine count too large! Setting to 10% of current board size ({total_mines}).")
+    elif len(sys.argv) > 1:
+        print("Wrong amount of arguments! Setting to defaults (10 rows, 10 columns, 10 mines).")
+
+
+    game_board = [[0 for x in range(cols)] for y in range(rows)]
+    game_board_is_revealed = [[0 for x in range(cols)] for y in range(rows)]
     game_board_map = [  pg.image.load('tiles/0.jpg'),
                         pg.image.load('tiles/1.jpg'),
                         pg.image.load('tiles/2.jpg'),
@@ -53,47 +66,48 @@ def main():
 
     # Populate board with mines
     mine_count = 0
-    while mine_count < 10:
-        x = rand.randint(0, rows-1)
-        y = rand.randint(0, columns-1)
-        if game_board[x][y] != 9:
-            game_board[x][y] = 9
+    while mine_count < total_mines:
+        row = rand.randint(0, rows-1)
+        col = rand.randint(0, cols-1)
+
+        if game_board[row][col] != 9:
+            game_board[row][col] = 9
             mine_count += 1
-    
+
     #Determine number of adjacent mines for each tile in the board
     for row in range(0, rows):
-        for column in range(0, columns):
+        for col in range(0, cols):
             mine_proximity_count = 0
-            if game_board[row][column] != 9:
+            if game_board[row][col] != 9:
                 if row-1 >= 0:
-                    if column-1 >= 0:
-                        if game_board[row-1][column-1] == 9:
+                    if col-1 >= 0:
+                        if game_board[row-1][col-1] == 9:
                             mine_proximity_count += 1
-                    if game_board[row-1][column] == 9:
+                    if game_board[row-1][col] == 9:
                         mine_proximity_count += 1
-                    if column+1 < columns:
-                        if game_board[row-1][column+1] == 9:
+                    if col+1 < cols:
+                        if game_board[row-1][col+1] == 9:
                                 mine_proximity_count += 1
-                if column-1 >= 0:
-                    if game_board[row][column-1] == 9:
+                if col-1 >= 0:
+                    if game_board[row][col-1] == 9:
                         mine_proximity_count += 1
-                if column+1 < columns:
-                    if game_board[row][column+1] == 9:
+                if col+1 < cols:
+                    if game_board[row][col+1] == 9:
                         mine_proximity_count += 1
                 if row+1 < rows:
-                    if column-1 >= 0:
-                        if game_board[row+1][column-1] == 9:
+                    if col-1 >= 0:
+                        if game_board[row+1][col-1] == 9:
                             mine_proximity_count += 1
-                    if game_board[row+1][column] == 9:
+                    if game_board[row+1][col] == 9:
                         mine_proximity_count += 1
-                    if column+1 < columns:
-                        if game_board[row+1][column+1] == 9:
+                    if col+1 < cols:
+                        if game_board[row+1][col+1] == 9:
                             mine_proximity_count += 1
-                game_board[row][column] = mine_proximity_count
+                game_board[row][col] = mine_proximity_count
     
     #Set up game window
     background_colour = (255,255,255)
-    (width, height) = (rows*50, columns*50)
+    (width, height) = (cols*50, rows*50)
 
     screen = pg.display.set_mode((width, height))
     pg.display.set_caption('PySweeper')
@@ -102,6 +116,7 @@ def main():
 
     mouse_states = [False, pg.mouse.get_pressed(num_buttons=3)[0]]
     clicked_mine = False
+    
     #Game loop
     while 1:
         pg.time.Clock().tick(30)
@@ -114,18 +129,18 @@ def main():
 
         for row in range(0, rows):
             x_pos = 0
-            for column in range(0, columns):
+            for col in range(0, cols):
                 if mouse_x > x_pos and mouse_x < x_pos+50 and mouse_y > y_pos and mouse_y < y_pos+50:
                     if(pg.mouse.get_pressed(num_buttons=3)[0]):
-                        if game_board[row][column] == 9:
+                        if game_board[row][col] == 9:
                             clicked_mine = True
-                        game_board_is_revealed = get_new_revealed_game_board(game_board, game_board_is_revealed, row, column, rows, columns)
+                        game_board_is_revealed = get_new_revealed_game_board(game_board, game_board_is_revealed, row, col, rows, cols)
                     else:                  
                         screen.blit(select ,(x_pos, y_pos))
-                elif game_board_is_revealed[row][column] == 0:
+                elif game_board_is_revealed[row][col] == 0:
                     screen.blit(cover ,(x_pos, y_pos))
                 else:
-                    screen.blit(game_board_map[game_board[row][column]], (x_pos, y_pos))
+                    screen.blit(game_board_map[game_board[row][col]], (x_pos, y_pos))
                 x_pos+=50
             y_pos+=50
         if clicked_mine:
@@ -134,7 +149,7 @@ def main():
             not_revealed = 0
             for row in game_board_is_revealed:
                 not_revealed += row.count(0)
-            if not_revealed == 10:
+            if not_revealed == total_mines:
                 screen.blit(win_text, (50, 100))
         pg.display.flip()
 
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # PySweeper
 
-MineSweeper made in Python using pygame.
+MineSweeper made in Python using PyGame.
 
-### Run
+## Run
 
-Use command:
+Prerequisite: PyGame is installed. You may get it from the official website.
+
+Using command:
 ```
 python3 PySweeper.py
 ```
+This command will create a board with 10 rows, 10 columns and 10 mines.
+
+## Modifying game settings
+
+The dimensions and mine count can be set using arguments.
+
+Example:
+```
+python3 PySweeper.py 12 10 15
+```
+This command will produce a board with 12 rows and 10 columns with 15 mines.
