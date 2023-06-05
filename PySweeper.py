0ff9cf7d27b8f38d2acb5f3ae43ee63ae147ed74
rnhkz <alb190010@utdallas.edu>
2023-06-05 15:36:37 -0500

    Added flags. Mouse clicks now register on release.

--- PySweeper.py
+++ PySweeper.py
@@ -7,13 +7,10 @@ from game_board import Game_Board
 pg.init()
 
 def main():
-    rows, cols = 10, 10
-    mines = 10
+    rows, cols, mines = 10, 10, 10
 
     if len(sys.argv) == 4:
-        rows = int(sys.argv[1])
-        cols = int(sys.argv[2])
-        mines = int(sys.argv[3])
+        rows, cols, mines = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
         if mines > (rows * cols) * .75:
             mines = int((rows * cols) * .1)
             print(f"Mine count too large! Setting to 10% of current board size ({mines}).")
@@ -34,6 +31,7 @@ def main():
                 pg.image.load('tiles/9.jpg')]
     cover = pg.image.load('tiles/cover.jpg')
     select = pg.image.load('tiles/select.jpg')
+    flag = pg.image.load('tiles/flag.jpg')
     win_text = pg.image.load('tiles/win.png')
     lose_text = pg.image.load('tiles/lose.png')
     
@@ -46,9 +44,10 @@ def main():
     screen.fill(background_colour)
     pg.display.flip()
 
-    mouse_states = [False, pg.mouse.get_pressed(num_buttons=3)[0]]
+    left_mouse_states = [False, pg.mouse.get_pressed(num_buttons=3)[0]]
+    right_mouse_states = [False, pg.mouse.get_pressed(num_buttons=3)[2]]
     clicked_mine = False
-    
+
     #Game loop
     while 1:
         pg.time.Clock().tick(30)
@@ -58,22 +57,28 @@ def main():
                 gb = Game_Board(rows, cols, mines)
                 clicked_mine = False
 
-        x_pos = 0
-        y_pos = 0
+        x_pos, y_pos = 0, 0
         mouse_x, mouse_y = pg.mouse.get_pos()
         keystate = pg.key.get_pressed()
-
+        left_mouse_states = [left_mouse_states[1], pg.mouse.get_pressed(num_buttons=3)[0]]
+        right_mouse_states = [right_mouse_states[1], pg.mouse.get_pressed(num_buttons=3)[2]]
         for row in range(0, rows):
             x_pos = 0
             for col in range(0, cols):
                 if mouse_x > x_pos and mouse_x < x_pos+50 and mouse_y > y_pos and mouse_y < y_pos+50:
-                    if(pg.mouse.get_pressed(num_buttons=3)[0]):
-                        if gb.map[row][col] == 9:
-                            clicked_mine = True
-                        gb.get_new_revealed_game_board(row, col)
+                    if(left_mouse_states == [True, False]):
+                        if gb.reveal_map[row][col] == 1:
+                            if gb.map[row][col] == 9:
+                                clicked_mine = True
+                            gb.get_new_reveal_map(row, col)
+                    elif(right_mouse_states == [True, False]):
+                        if gb.reveal_map[row][col] != 0:
+                            gb.change_flag(row, col)
                     else:
                         screen.blit(select ,(x_pos, y_pos))
-                elif gb.is_revealed_map[row][col] == 0:
+                elif gb.reveal_map[row][col] == -1:
+                    screen.blit(flag ,(x_pos, y_pos))
+                elif gb.reveal_map[row][col] == 1:
                     screen.blit(cover ,(x_pos, y_pos))
                 else:
                     screen.blit(gb_tiles[gb.map[row][col]], (x_pos, y_pos))
@@ -83,8 +88,8 @@ def main():
             screen.blit(lose_text, (50, 100))
         else:
             not_revealed = 0
-            for row in gb.is_revealed_map:
-                not_revealed += row.count(0)
+            for row in gb.reveal_map:
+                not_revealed += cols - row.count(0)
             if not_revealed == mines:
                 screen.blit(win_text, (50, 100))
         pg.display.flip()
--- game_board.py
+++ game_board.py
@@ -6,7 +6,7 @@ class Game_Board:
         self.cols = cols
         self.mines = mines
         self.map = self.generate_map()
-        self.is_revealed_map = [[0 for x in range(cols)] for y in range(rows)]
+        self.reveal_map = [[1 for x in range(cols)] for y in range(rows)]
     
     def generate_map(self):
         temp_map = [[0 for x in range(self.cols)] for y in range(self.rows)]
@@ -50,16 +50,15 @@ class Game_Board:
                             if temp_map[row+1][col+1] == 9:
                                 mine_proximity_count += 1
                     temp_map[row][col] = mine_proximity_count
-        
         return temp_map
 
-    def get_new_revealed_game_board(self, row, col):
+    def get_new_reveal_map(self, row, col):
         stack = [[row, col]]
         while len(stack) > 0:
             cur_tile = stack.pop()
             row=cur_tile[0]
             col=cur_tile[1]
-            if self.is_revealed_map[row][col] == 0:
+            if self.reveal_map[row][col] == 1:
                 if self.map[row][col] == 0:
                     if row-1 >= 0:
                         if col-1 >= 0:
@@ -77,5 +76,9 @@ class Game_Board:
                         if row+1 < self.rows:
                             stack.append([row+1, col-1])
                         stack.append([row, col-1])
-                    self.is_revealed_map[row][col] = 1
-            self.is_revealed_map[row][col] = 1
+                    self.reveal_map[row][col] = 0
+            if self.reveal_map[row][col] != -1:
+                self.reveal_map[row][col] = 0
+
+    def change_flag(self, row, col):
+        self.reveal_map[row][col] = self.reveal_map[row][col] * -1
